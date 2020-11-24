924260c09d05ce38d96669ed1a91679bb99e8e8f
zorva <you@example.com>
2020-11-24 17:23:06 -0500

    enabling JSX from an external file

---
+++ src/Rc5.js
@@ -0,0 +1,9 @@
+'use strict';
+
+class Title extends React.Component {
+  render() {
+    return (
+    <h1>Hello world!</h1>)
+  }
+};
+ReactDOM.render(<Title />, document.getElementById('root')); 
