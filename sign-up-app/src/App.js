0c9d6c619684b25c492f4076b86789bfe8d2b2f2
zorva <you@example.com>
2021-02-21 18:39:42 -0500

    single sign up webpage

---
+++ sign-up-app/src/App.js
@@ -0,0 +1,15 @@
+function App() {
+  return (
+    <div className="App">
+      <form className="mainForm">
+        <label htmlFor="UserName">User Name</label>
+        <input type="text" id="UserName"></input>
+        
+        <label htmlFor="Email">Email</label>
+        <input type="text" id="Email"></input>
+      </form>
+    </div>
+  );
+}
+
+export default App;
