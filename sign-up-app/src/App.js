2a5d9f4b66a3dc1fc2badb5aae2bb788a63e388e
zorva <you@example.com>
2021-02-24 18:11:40 -0500

    improving CSS skills with practice

--- sign-up-app/src/App.js
+++ sign-up-app/src/App.js
@@ -2,11 +2,14 @@ function App() {
   return (
     <div className="App">
       <form className="mainForm">
+      <div id ="identification">
         <label htmlFor="UserName">User Name</label>
         <input type="text" id="UserName"></input>
-        
-        <label htmlFor="Email">Email</label>
-        <input type="text" id="Email"></input>
+        </div>
+        <div id="contact">
+        <label htmlFor="mail">Email</label>
+        <input type="email" id="mail"></input>
+        </div>
       </form>
     </div>
   );
